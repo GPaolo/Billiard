ad3f0517dc851ff70b7262b3590e9fc20efb919e
Giuseppe <giuseppe.paolo@softbankrobotics.com>
2019-08-20 12:45:42 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
+# Created by Giuseppe Paolo 
+# Date: 21/12/18
+
 from setuptools import setup
 
 setup(name='gym_billiard',
       version='0.0.1',
       install_requires=['gym', 'numpy', 'Box2D', 'pygame']
+      author="Giuseppe Paolo",
+      author_email="giuseppe.paolo93@gmail.com",
+      description="An OpenAI gym environment",
 )
