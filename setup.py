6d7a5bfae025146cd886229685aed816d2af6286
Giuseppe Laptop <giuseppe.paolo93@gmail.com>
2020-01-16 18:27:38 +0100

    Solved typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(name='gym_billiard',
       version='1',
-      install_requires=['gym', 'numpy', 'box2d-py', 'pygame']
+      install_requires=['gym', 'numpy', 'box2d-py', 'pygame'],
       author="Giuseppe Paolo",
       author_email="giuseppe.paolo93@gmail.com",
       description="An OpenAI gym environment",
